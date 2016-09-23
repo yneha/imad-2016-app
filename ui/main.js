2d3957170e2226d22302962a41edea1e8b9a9456
yneha <neharao594@gmail.com>
2016-09-23 20:44:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var im=document.getElementById('img');
+var lft=0;
+function move(){
+    lft=lft+2;
+    img.style.lft=lft+'px';
+}
+im.onclick=function(){
+    var int=setInterval(move,100);
+}
