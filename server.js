c321a958b642c793dec5c8b73737c02da7af7600
yneha <neharao594@gmail.com>
2016-09-23 20:30:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,10 +4,49 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var custom={
+title:'Custom',
+heading:'Gifts',
+content:`<p>Welcome To Made4U Gift Shop </p>`
+
+};
+function createtemp(data){
+var title=data.title;
+var heading=data.heading;
+var content=data.content;
+var temp=
+`<!doctype html>
+<html>
+    <head>
+<title>${title}
+</title>
+	${heading}
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="center">
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+           ${content}
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>`;
+return temp;
+
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/custom', function (req, res) {
+  res.send(createtemp(custom));
+});
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
